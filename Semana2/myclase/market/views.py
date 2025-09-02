46bf52648f207ec201e15baf941cdaf8bc38dced
ematevez <ematevez@gmail.com>
2025-09-02 16:56:47 -0300

    Semana2

---
+++ Semana2/myclase/market/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Product
+
+
+def product_list(request):
+    products = Product.objects.filter(active=True).order_by("-created_at")
+    return render(request, "product_list.html", {"products": products})
+
